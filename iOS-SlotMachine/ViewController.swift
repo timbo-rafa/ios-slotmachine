e545f0a436da7d3a63be2afe506d7ca22554d45c
Student <student@setass-mac-mini-3.local>
2018-01-23 20:18:32 -0500

    Initial Commit

---
+++ iOS-SlotMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-SlotMachine
+//
+//  Created by Student on 2018-01-23.
+//  Copyright © 2018 Rafael Matos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
