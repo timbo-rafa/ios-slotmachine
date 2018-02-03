ccbac794c31e956975c6aaf86ed8c4a8fe0ce499
SergioBrunacci <sergio.a.brunacci@gmail.com>
2018-02-03 15:38:45 -0500

    Implement the functionality to suspend the application

--- iOS-SlotMachine/ViewController.swift
+++ iOS-SlotMachine/ViewController.swift
@@ -87,6 +87,7 @@ class ViewController: UIViewController, UIPickerViewDelegate {
     
     
     @IBAction func pressQuit(_ sender: UIButton) {
+        //suspend the app in second plan
         UIControl().sendAction(#selector(NSXPCConnection.suspend), to: UIApplication.shared, for: nil)
     }
     
