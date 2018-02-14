8af63d48debe71f31ac0f00ac222fe45fd4cda5d
Student <student@setass-mac-mini-20.local>
2018-02-13 19:54:33 -0500

    engine reworked

--- iOS-SlotMachine/SlotMachineEngine.swift
+++ iOS-SlotMachine/SlotMachineEngine.swift
@@ -17,14 +17,13 @@ class SlotMachineEngine {
     var money: Int
     var bet: Int
     var bonus: Int
+    var matchedFruit: Int
     
     var disableFive: Bool
     var disableFifty: Bool
     var nullBet: Bool
     var isJackpot: Bool
     var fly: Bool
-    
-    var match: String
     
     var sounds: Sound = Sound()
     var random: Random = Random()
@@ -45,6 +44,7 @@ class SlotMachineEngine {
         self.money = 0
         self.bet = 0
         self.bonus = 0
+        self.matchedFruit = self.emptyRow
         
         self.disableFive = true
         self.disableFifty = true
@@ -53,12 +53,6 @@ class SlotMachineEngine {
         self.fly = false
         
         sounds.Start()
-    }
-    
-
-    
-    public func spin() {
-        spinReels()
     }
     
     public func reset() {
@@ -91,6 +85,11 @@ class SlotMachineEngine {
         self.fly = false
     }
     
+    public func resetBet() {
+        self.bet = 0
+        self.nullBet = true
+    }
+    
     public func bet(Value value: Int) -> Bool {
         // Only bet what you have
         if (value > self.money) {
@@ -112,98 +111,90 @@ class SlotMachineEngine {
         return true
     }
     
+    public func spin() {
+        spinReels()
+        calculateBonus()
+    }
+    
     private func spinReels() {
-        
-        //Spin Sound
-        //spinPlayer.play()
-
         self.selected1 = rand()
         self.selected2 = rand()
         self.selected3 = rand()
         self.selected4 = rand()
         self.selected5 = rand()
+    }
+    
+    private func calculateBonus() {
+        self.resetBonus()
+        self.resetFly()
         
-        //let's reduce var names to simplify the code :-)
-        let s1 = self.selected1
-        let s2 = self.selected2
-        let s3 = self.selected3
-        let s4 = self.selected4
-        let s5 = self.selected5
+        if (!self.fliesSelected()) {
+            self.matchFruits()
+            
+            if (bonus > 0) {
+                self.money += bonus
+            }
+        }
+        self.resetBet()
         
-        /**** RULES *****************************************
-         * 1. A fly in any position, no bonus (fly = index 0)
-         * 2. 3 equals with 2 gaps, receive 1x bet back
-         * 3. 3 equals with 1 gap, receive 2x bet back
-         * 4. 3 equals in a row, receive 3x bet back
-         * 5. 5 equals in a row, receive jackpot back
-         ****************************************************/
-
-        bonus = 0
-
-        // check for flies (first rows)
-        if (s1 >= fliesCount &&
-            s2 >= fliesCount &&
-            s3 >= fliesCount &&
-            s4 >= fliesCount &&
-            s5 >= fliesCount) {
-            
-            if (s1 == s2 && s2 == s3 && s3 == s4 && s4 == s5) {
-              
-                self.money += self.jackpot     // ***** JACKPOT WINNER *****
-                bonus = self.jackpot
-                self.isJackpot = true
-            } else {
-                
-                checkForRepeatedFruits()
-
-                if (bonus > 0) {
-                    self.money += bonus
-                    //sounds.shortPayOutPlayer.play()
-                }else{
-                    //sounds.lostPlayer.play()
-                }
-            
-                self.bet = 0
-                self.nullBet = true
-                
-            } // if (s1 == s2  && s2 == s3 && s3 == s4 && s4 == s5)
-            
-        } else {
-            // Fly came
-            self.bet = 0
-            self.nullBet = true
-            self.fly = true
-            
-        } // if (s1 > 0 && s2 > 0 && s3 > 0 && s4 > 0 && s5 > 0)
-
         print("Bonus = ", bonus)  // write bonus in the log
     }
     
-    private func checkForRepeatedFruits() {
-        // compares selected1 (s1) value with the second, third, forth and fifth reels
-        let pivot1 = checkMatch(self.selected1)
-        pivotCompare(pivot1)
-        
-        // compares selected2 (s2) value with the first, third, forth and fifth reels
-        let pivot2 = checkMatch(self.selected2)
-        pivotCompare(pivot2)
-        
-        // compares selected3 (s3) value with the first, second, forth and fifth reels
-        let pivot3 = checkMatch(self.selected3)
-        pivotCompare(pivot3)
+    private func fliesSelected() -> Bool {
+        // check for flies (first rows)
+        self.fly = false
+        if (self.selected1 < self.fliesCount) { self.fly = true }
+        if (self.selected2 < self.fliesCount) { self.fly = true }
+        if (self.selected3 < self.fliesCount) { self.fly = true }
+        if (self.selected4 < self.fliesCount) { self.fly = true }
+        if (self.selected5 < self.fliesCount) { self.fly = true }
+        return self.fly
     }
     
-    private func pivotCompare(_ pivot: String) {
-        if ( pivot == "TTTFF" || pivot == "FTTTF" || pivot == "FFTTT" ) {
-            bonus = self.bet * 3
-            self.match = pivot
-        } else if ( pivot == "TFTTF" || pivot == "FTTFT" || pivot == "TTFTF" || pivot == "TFTFT" || pivot == "FTFTT" ) {
-            bonus = self.bet * 2
-            self.match = pivot
-        } else if ( pivot == "TFFTT" || pivot == "TTFFT" ) {
-            bonus = self.bet * 1
-            self.match = pivot
+    private func matchFruits() {
+        matchFruit(self.selected1)
+        matchFruit(self.selected2)
+        matchFruit(self.selected3)
+        // since 3 or more fruits are needed,
+        // we do not need to compare for
+        // selected4 and selected5
+    }
+    
+    private func matchFruit(_ fruit: Int) {
+        let count = matches(fruit)
+        awardRepeatedFruits(count)
+        if (count > 2) {
+            self.matchedFruit = fruit
         }
+    }
+    
+    private func awardRepeatedFruits(_ count: Int) {
+        if (count == 5) {
+            self.money += self.jackpot
+            bonus = self.jackpot
+            self.isJackpot = true
+        } else if (count == 4) {
+            bonus = self.bet * 3
+        } else if (count == 3) {
+            bonus = self.bet * 2
+        }
+        //no award for 2 or 1 repeated fruits
+    }
+    
+    private func matches(_ fruit: Int) -> Int {
+        // returns how many fruits there are equal to the passed one.
+        var count: Int = 0
+        if (isSameFruit(self.selected1, fruit)) { count = count + 1 }
+        if (isSameFruit(self.selected2, fruit)) { count = count + 1 }
+        if (isSameFruit(self.selected3, fruit)) { count = count + 1 }
+        if (isSameFruit(self.selected4, fruit)) { count = count + 1 }
+        if (isSameFruit(self.selected5, fruit)) { count = count + 1 }
+        return count
+    }
+    
+    private func isSameFruit(_ fruit1: Int, _ fruit2: Int) -> Bool {
+        if fruit1 == fruit2 { return true }
+        else { return false }
     }
     
     private func rand() -> Int {
@@ -224,19 +215,6 @@ class SlotMachineEngine {
         self.selected3 = row
         self.selected4 = row
         self.selected5 = row
-    }
-    
-    private func checkMatch( _ x: Int) -> String {
-        var y = ""
-        if self.selected1 == x { y = y + "T" } else { y = y + "F"}
-        if self.selected2 == x { y = y + "T" } else { y = y + "F"}
-        if self.selected3 == x { y = y + "T" } else { y = y + "F"}
-        if self.selected4 == x { y = y + "T" } else { y = y + "F"}
-        if self.selected5 == x { y = y + "T" } else { y = y + "F"}
-        //play fly - lost everything
-        //flyPlayer.play()
-        return y
-        
     }
 }
 
